0a42d36ab64ea8f9a1e7b7f1baa34afb0980f3b8
Kefei Dan Zhou <kefei.zhou@gmail.com>
2010-12-01 22:47:28 -0500

    solved project euler #312

--- problem312.py
+++ problem312.py
@@ -1,11 +1,27 @@
-# I worked out most of the problem with pen/paper
-# iterative formula
-# k(1) = 1
-# k(n+1)= 3*[k(n-1)**3]
-# C(n) = k(n)**3
-#
-# and cycle lenth of C is 28960854
-#
+"""
+Project Euler 312 
+
+- A Sierpinski graph of order-1 (S1) is an equilateral triangle.
+- Sn+1 is obtained from Sn by positioning three copies of Sn so that every pair of copies has one common corner.
+
+Let C(n) be the number of cycles that pass exactly once through all the vertices of Sn.
+For example, C(3) = 8 because eight such cycles can be drawn on S3, as shown below:
+
+It can also be verified that :
+C(1) = C(2) = 1
+C(5) = 71328803586048
+C(10 000) mod 108 = 37652224
+C(10 000) mod 138 = 617720485
+Find C(C(C(10 000))) mod 138.
+
+I worked out most of the problem with pen/paper
+iterative formula: 
+    k(1) = 1
+    k(n+1)= 3*[k(n-1)**3]
+    C(n) = k(n)**3
+    
+and cycle lenth of C is 28960854
+"""
 
 def find_cycle(mod=13**8):
     k = 24 
