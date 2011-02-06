f7fcee1088f7a8befcc69cb7b3d226acb9cef4ad
Kefei Dan Zhou <kefei.zhou@gmail.com>
2011-02-06 12:31:05 -0500

    simplify solution PE323

--- problem323.py
+++ problem323.py
@@ -5,13 +5,13 @@ from fractions import Fraction
 def problem323():
     """
     Let y0, y1, y2,... be a sequence of random unsigned 32 bit integers
-    (i.e. 0  yi  232, every value equally likely).
+    (i.e. 0  yi  2^32, every value equally likely).
 
     For the sequence xi the following recursion is given:
     x0 = 0 and
     xi = xi-1 | yi-1, for i  0. ( | is the bitwise-OR operator)
     It can be seen that eventually there will be an index N such that 
-        xi = 232 -1 (a bit-pattern of all ones) for all i  N.
+        xi = 2^32 -1 (a bit-pattern of all ones) for all i  N.
 
     Find the expected value of N. 
     Give your answer rounded to 10 digits after the decimal point.
@@ -23,17 +23,17 @@ def problem323():
     # ..
 
     #expected number of steps for n-bit
-    esteps = [0, Fraction(2,1)] + [0]*100
+    esteps = [0] * 33
 
     #for binomial coefficients
     def pascal_generator():
-        row = [1,1]
+        row = [1]
         while True:
             row = [1] + [row[i]+row[i+1] for i in range(0,len(row)-1)] + [1]
             yield row 
 
     pascal = pascal_generator()
-    for level in range(2, 33):
+    for level in range(1, 33):
         row = pascal.next()
         esteps[level] = (
             Fraction(2**level, 2**level-1) * 
